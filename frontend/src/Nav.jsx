c3a2453fe18c94519edace0fd116cf0264ef3e00
RyunK, Lee <lrk222@naver.com>
2026-07-31 14:29:32 +0900

    chore: 앱 제목 (nav바) 변경

--- frontend/src/Nav.jsx
+++ frontend/src/Nav.jsx
@@ -4,7 +4,7 @@ import { backendUrl } from './backendUrl'
 function Nav() {
   return (
     <header>
-      <h1><Link to="/">⚔️ 전투 GM 계산기</Link></h1>
+      <h1><Link to="/">⚔️ GM을 위한 전투 계산기</Link></h1>
       <nav>
         <a href={backendUrl('/groups')}>그룹</a>
         <Link to="/characters">캐릭터</Link>
