eee774a259c9a0bf09aaad2f2df4b25d03ac6163
RyunK, Lee <lrk222@naver.com>
2026-08-12 16:46:24 +0900

    chore: 색상과 앱 이름 변경

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { backendUrl } from '../utils/backendUrl'
 function Nav() {
   return (
     <header>
-      <h1><Link to="/">⚔️ GM을 위한 전투 계산기</Link></h1>
+      <h1><Link to="/">🍊 감귤 전투 계산기</Link></h1>
       <nav>
         {/* <a href={backendUrl('/groups')}>그룹</a> */}
         <Link to="/groups">그룹</Link>
