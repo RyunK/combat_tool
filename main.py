996b5309ccdba6aad8c157d7c644712883f00f73
RyunK, Lee <lrk222@naver.com>
2026-08-19 15:08:59 +0900

    chore: 백 변경시 자동 반영되게

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ def _open_browser():
 
 if __name__ == "__main__":
     threading.Timer(1.2, _open_browser).start()
-    uvicorn.run(app, host="127.0.0.1", port=8901, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8901, reload=True)
